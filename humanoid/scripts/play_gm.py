caae92849d091cfeb82ee94daf1efda612bfa1cd
cuizeyu <cuizeyu@weichai.com>
2026-08-12 17:29:59 +0800

    exp0.19: update play checkpoint URL to model_2000

--- humanoid/scripts/play_gm.py
+++ humanoid/scripts/play_gm.py
@@ -24,7 +24,7 @@ from humanoid.utils import get_args, export_policy_as_jit, task_registry, Logger
 from isaacgym.torch_utils import *
 
 # Fallback: download checkpoint from OSS if not found locally
-FALLBACK_CHECKPOINT_URL = "https://limx-gradmotion.oss-cn-beijing.aliyuncs.com/upload%2F2026%2F8%2F12%2Fmodel_3000_20260812102153A256.pt?OSSAccessKeyId=LTAI5tMec8RQN1nZuRkVMgxz&Expires=1787106360&Signature=BEzjt9gIKOaMC7byPZrilc10xZw%3D"
+FALLBACK_CHECKPOINT_URL = "https://limx-gradmotion.oss-cn-beijing.aliyuncs.com/upload%2F2026%2F8%2F12%2Fmodel_2000_20260812171702A745.pt?OSSAccessKeyId=LTAI5tMec8RQN1nZuRkVMgxz&Expires=1787131772&Signature=qfXN2YxzIrj9cNNPbOQ38KmHoZg%3D"
 
 
 def find_checkpoint():
