15a867639af21f48469ce0ab4252e5fde43e61c7
cuizeyu <cuizeyu@weichai.com>
2026-08-12 08:50:23 +0800

    chore: point play fallback checkpoint to exp0.15 model_3800

--- humanoid/scripts/play_gm.py
+++ humanoid/scripts/play_gm.py
@@ -24,7 +24,7 @@ from humanoid.utils import get_args, export_policy_as_jit, task_registry, Logger
 from isaacgym.torch_utils import *
 
 # Fallback: download checkpoint from OSS if not found locally
-FALLBACK_CHECKPOINT_URL = "https://limx-gradmotion.oss-cn-beijing.aliyuncs.com/upload%2F2026%2F8%2F12%2Fmodel_5300_20260812074655A743.pt?OSSAccessKeyId=LTAI5tMec8RQN1nZuRkVMgxz&Expires=1787097509&Signature=OF0hkHa%2Bwj9cHHZ%2BUMh3wYPkTt8%3D"
+FALLBACK_CHECKPOINT_URL = "https://limx-gradmotion.oss-cn-beijing.aliyuncs.com/upload%2F2026%2F8%2F12%2Fmodel_3800_20260812084925A727.pt?OSSAccessKeyId=LTAI5tMec8RQN1nZuRkVMgxz&Expires=1787100600&Signature=aov6b59xkfoV%2FP7%2Bfw0%2Bpu7nEQI%3D"
 
 
 def find_checkpoint():
